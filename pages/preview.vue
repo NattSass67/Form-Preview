5042403091dca718ffd0e64ec11cc88b8003a39c
NattSass67 <nattanun.sk@gmail.com>
2024-10-30 01:02:12 +0700

    fix-design

--- pages/preview.vue
+++ pages/preview.vue
@@ -1,11 +1,12 @@
 <script setup>
-import FormRenderer from '~/components/preview/FormRenderer.vue'
+// import FormRenderer from '~/components/preview/FormRenderer.vue'
 
 provide('id', ref())
 </script>
 
 <template>
   <main class="space-y-6">
-    <FormRenderer />
+    <DashboardBreadcrumb title="Analysis" />
+    <!-- <FormRenderer /> -->
   </main>
 </template>
